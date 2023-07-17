5e1f1f3a9cc33739c3af3dfd5ec6cc958540622d
Michał Jasiński <jasinskimichal.1996@gmail.com>
2023-07-17 10:33:29 +0200

    database connection - db - "wallet"

--- packages/backend/server.js
+++ packages/backend/server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 const Port = process.env.PORT;
 
-mongoose.connect(process.env.DB_HOST, {
+mongoose.connect(`${process.env.MONGO_URI}`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
