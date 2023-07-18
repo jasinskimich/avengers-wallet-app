eb7642b20aaabd5c3a754a7b2f5e320b36e5deb4
Qube24 <k.twerdochlib@gmail.com>
2023-07-18 19:56:24 +0200

    navbar + routes + fix .json dependencies

---
+++ packages/frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+
+function Home() {
+	return (
+		<Box>
+			<h1>Home</h1>
+			<p>Path to register or login "/register" , "/login"</p>
+		</Box>
+	);
+}
+
+export default Home;
