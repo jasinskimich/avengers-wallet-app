110527c0d5db1866013b07dd889c7566a4992421
Qube24 <k.twerdochlib@gmail.com>
2023-07-18 19:55:28 +0200

    navbar and routers

--- packages/frontend/src/index.js
+++ packages/frontend/src/index.js
@@ -1,14 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: "#FFFFFF",
+			secondary: "#4A56E2",
+			green: "#24CCA7",
+			red: "FF6596",
+		},
+		text: {
+			standard: "#00838f",
+			gray: "#BDBDBD",
+		},
+	},
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<BrowserRouter basename="/">
+			<ThemeProvider theme={theme}>
+				<App />
+			</ThemeProvider>
+		</BrowserRouter>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ packages/frontend/src/pages/LoginPages/LoginPages.jsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+
+function Login() {
+	return (
+		<Box>
+			<h1>Login</h1>
+		</Box>
+	);
+}
+
+export default Login;
