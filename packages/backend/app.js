5b2c0869dd18d95d7997c7e244c03d18b76edcff
Matjelko <matjelko11@gmail.com>
2023-07-18 13:41:16 +0200

    add-endpoints-for-users

--- packages/backend/app.js
+++ packages/backend/app.js
@@ -1,8 +1,8 @@
-require("dotenv").config();
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 
+const usersRouter = require('./routes/users')
 
 const app = express()
 
@@ -12,7 +12,9 @@ app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
 
+require('./config/config-passport')
 
+app.use(usersRouter)
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
