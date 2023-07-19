f8675a4d194b62ff56707e6e84a2b7244c711786
OliwiaWawrzynek <oliwiawawrzynek@gmail.com>
2023-07-19 20:58:03 +0200

    RegistrationPages
    mobile, tablet, desktop

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -1,19 +1,19 @@
 import "./App.css";
 import "./stylesheet/fonts.css";
-import RegistrationForm from "./components/RegistrationForm/RegistrationForm";
 import Layout from "./Layout";
 import { Routes, Route } from "react-router-dom";
 import Statistics from "./pages/Statistics/Statistics";
 import Home from "./pages/Home/Home";
 import Login from "./pages/LoginPages/LoginPages";
 import MobileTable from "./pages/MobileTable/MobileTable";
+import RegistrationPages from "./pages/RegistrationPages/RegistrationPages";
 
 function App() {
 	return (
 		<div className="App">
 			<Routes>
 				Wallet App
-				<Route path="/register" element={<RegistrationForm />} />
+				<Route path="/register" element={<RegistrationPages />} />
 				<Route path="/login" element={<Login />} />
 				<Route path="/" element={<Layout />}>
 					<Route path="/home" element={<Home />} />
