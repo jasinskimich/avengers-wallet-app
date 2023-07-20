b5fd7ead97c03306208f9f210c97fab718262645
Sławomir Albiński <slawomiralbinski@gmail.com>
2023-07-20 13:00:57 +0200

    Update LoginForm.jsx

--- packages/frontend/src/components/LoginForm/LoginForm.jsx
+++ packages/frontend/src/components/LoginForm/LoginForm.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate} from "react-router-dom";
 
 const LoginForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
+  const navigate = useNavigate();
+  
   const handleInputChange = (e) => {
     const { id, value } = e.target;
     if (id === "email") {
@@ -39,6 +41,8 @@ const LoginForm = () => {
         setEmail("");
         setPassword("");
         setError("");
+        
+        navigate("/home");
       } else {
         const errorData = await response.json();
         setError(errorData.message);
