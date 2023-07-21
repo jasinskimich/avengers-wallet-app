02906c1c95219295c127614e1fd10656099afd74
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2023-07-21 02:20:12 +0200

    create Currency component

---
+++ packages/frontend/src/components/Currency/Currency.jsx
@@ -0,0 +1,78 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { selectCurrencies } from 'redux/currency/selectors';
+import { getCurrency } from '../../redux/currency/operations';
+import { CircularIndeterminate } from 'components/Spinner/CircularIndeterminate';
+import css from './Currency.module.scss';
+
+export const Currency = () => {
+  const dispatch = useDispatch();
+  const date = new Date();
+
+  const currencyValues = () => {
+    const localHour = localStorage.getItem('hasHourPassed');
+    if (localHour !== null && date.getTime() / 1000 - localHour <= 3600) {
+      return;
+    }
+    const currencies = dispatch(getCurrency());
+    const hasHourPassed = date.getTime() / 1000;
+    localStorage.setItem('hasHourPassed', hasHourPassed);
+    return currencies;
+  };
+
+  currencyValues();
+
+  const values = useSelector(selectCurrencies);
+
+  return values.length === 0 ? (
+    <div className={css.currencyTableWrapper}>
+      <table className={css.currencyTable}>
+        <div className={css.spinnerBox}>
+          <CircularIndeterminate />
+        </div>
+        <thead className={css.currencyTableHead}>
+          <tr>
+            <th>Currency</th>
+            <th>Purchase</th>
+            <th>Sale</th>
+          </tr>
+        </thead>
+        <tbody className={css.currencyTableBody}>
+          <tr>
+            <td>USD</td>
+            <td>000</td>
+            <td>000</td>
+          </tr>
+          <tr>
+            <td>EUR</td>
+            <td>000</td>
+            <td>000</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  ) : (
+    <div className={css.currencyTableWrapper}>
+      <table className={css.currencyTable}>
+        <thead className={css.currencyTableHead}>
+          <tr>
+            <th>Currency</th>
+            <th>Purchase</th>
+            <th>Sale</th>
+          </tr>
+        </thead>
+        <tbody className={css.currencyTableBody}>
+          <tr>
+            <td>USD</td>
+            <td>{values[0].ask}</td>
+            <td>{values[0].bid}</td>
+          </tr>
+          <tr>
+            <td>EUR</td>
+            <td>{values[1].ask}</td>
+            <td>{values[1].bid}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
--- packages/frontend/src/redux/currency/currencySlice.js
+++ packages/frontend/src/redux/currency/currencySlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { getCurrency } from "./operations";
+import { getCurrency } from "./currencyThunk";
 
 const initialState = { error: "null", currencies: [] };
 
