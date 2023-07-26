c4c5998ce14b23605c134842f7ff9160567bd783
Matjelko <matjelko11@gmail.com>
2023-07-26 15:57:47 +0200

    verify-stylization

--- packages/frontend/src/components/VerifyForm/VerifyForm.jsx
+++ packages/frontend/src/components/VerifyForm/VerifyForm.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
 import Notiflix from "notiflix";
+import { Box, FormControl, InputAdornment, Input } from "@mui/material";
+import LocalPostOfficeIcon from '@mui/icons-material/LocalPostOffice';
+import walletIcon from "../../images/Wallet.svg"
+import { Link } from "react-router-dom";
+import css from "./VerifyForm.module.css"
 
 const VerifyForm = () => {
     const [email, setEmail] = useState("");
@@ -55,19 +60,50 @@ const VerifyForm = () => {
     };
   
     return (
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Email </label>
-          <input 
-            type="email" 
-            id="email" 
-            value={email} 
-            onChange={(e) => handleInputChange(e)} 
-            placeholder="Email" />
-        </div>
-        <button type="submit">
-          Send verification link again.
-        </button>
+      <form onSubmit={handleSubmit} className={css.verifyForm}>
+        <Box sx={{ '& > :not(style)': { m: 2 } }} className={css.verifyBox}>
+          <div className={css.verifyFormHeader}>
+            <img src={walletIcon} alt="wallet-icon" className={css.walletIcon} />
+            <h1>Wallet</h1>
+          </div>
+
+          <FormControl variant="standard" className={css.inputWidth}>
+            <Input 
+              type="email" 
+              id="email" 
+              value={email} 
+              onChange={(e) => handleInputChange(e)} 
+              placeholder="E-mail"
+              required
+              className={css.inputMargin}
+              startAdornment={
+                <InputAdornment position="start">
+
+                  <LocalPostOfficeIcon sx={{ color: 'lightgrey', mr: 1, my: 0.5 }} className={css.iconMarginPost} />
+
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+
+          <button variant="contained" type="submit" className={css.verifyButton}>
+            Send verification link again
+          </button>
+
+          <div className={css.verifyLinkSection}>
+            <div>
+            <Link to="/login" className={css.verifyLink}>
+              Login page
+            </Link>
+            </div>
+
+            <div className={css.verifyLinkMargin}>
+            <Link to="/register" className={css.verifyLink}>
+              Back to register page
+            </Link>
+            </div>
+          </div>
+        </Box>
       </form>
     );
 };
