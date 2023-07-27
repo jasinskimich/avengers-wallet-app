e11cf6e5c5317f9b2a29d1c9c3de66189ddac7ee
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2023-07-27 22:19:48 +0200

    update Header

--- packages/frontend/src/Layout.js
+++ packages/frontend/src/Layout.js
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
-import AppBarMain from "./components/AppBarMain/AppBarMain";
+// import AppBarMain from "./components/AppBarMain/AppBarMain";
+import { Header } from "./components/Header/Header";
 import { Suspense } from "react";
 import Navigation from "./components/Navigation/Navigation";
 
 function Layout() {
 	return (
 		<div>
-			<AppBarMain />
+			<Header/>
 			<Navigation />
 			<Suspense fallback={null}>
 				<Outlet />
