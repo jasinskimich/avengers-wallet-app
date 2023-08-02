990a12f64f47f629aaabd0ea081eeeb4d017f4cb
PatrycjaPiwowarczyk <piwowarczyk.p.a@gmail.com>
2023-08-03 01:25:11 +0200

    up

--- packages/frontend/src/components/BalanceSettingsModal/BalanceSettingsModal.js
+++ packages/frontend/src/components/BalanceSettingsModal/BalanceSettingsModal.js
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import css from "./BalanceSettingsModal.module.css";
 import CurrencyForm from "../CurrencyForm/CurrencyForm";
 
-
 function BalanceSettingsModal({ setSettingsOpenModal, updateCurrency }) {
   const handleCloseModal = () => {
     setSettingsOpenModal(false);
@@ -20,20 +19,22 @@ function BalanceSettingsModal({ setSettingsOpenModal, updateCurrency }) {
     return () => {
       document.removeEventListener("keydown", handleKeyDown);
     };
-  },);
+  });
 
   return (
-    <div className={css.modalBackground}>
-      <div className={css.modalContainer}>
-        <div className={css.titleCloseBtn}>
-          <button onClick={handleCloseModal}>x</button>
-        </div>
-        <h2 className={css.modalHeader}>Change your currency</h2>
+    <div className={css.modalBox}>
+      <div className={css.modalBackground}>
+        <div className={css.modalContainer}>
+          <div className={css.titleCloseBtn}>
+            <button onClick={handleCloseModal}></button>
+          </div>
+          <h2 className={css.modalHeader}>Change your currency</h2>
 
-        <CurrencyForm updateCurrency={updateCurrency} handleCloseModal={handleCloseModal} />
-        <div>
-          <div className={css.modalFooter}>
-            <button onClick={handleCloseModal}>Cancel</button>
+          <CurrencyForm updateCurrency={updateCurrency} handleCloseModal={handleCloseModal} />
+          <div>
+            <div className={css.modalFooter}>
+              <button onClick={handleCloseModal}>Cancel</button>
+            </div>
           </div>
         </div>
       </div>
--- packages/frontend/src/components/TransactionEditModal/EditModal.js
+++ packages/frontend/src/components/TransactionEditModal/EditModal.js
@@ -3,12 +3,12 @@ import css from "./EditModal.module.css";
 import { useState } from "react";
 import SwitchCheckbox from "../SwitchCheckbox/SwitchCheckbox";
 import Modal from "react-modal";
+import { Header } from "../Header/Header";
 
 Modal.setAppElement("#root");
 
-
 function EditModal({ setOpenEditModal, id, updateBalance, updateTransactions, prevSum, prevType, prevComment, prevCategory }) {
-    const [checked, setChecked] = useState(false);
+  const [checked, setChecked] = useState(false);
 
   const handleToggle = () => {
     setChecked(!checked);
@@ -18,45 +18,27 @@ function EditModal({ setOpenEditModal, id, updateBalance, updateTransactions, pr
   };
 
   return (
-    <Modal
-      isOpen={true} 
-      onRequestClose={handleCloseModal} 
-      className={css.editModalBackground} 
-      overlayClassName={css.editModalOverlay} 
-    >
-      <div className={css.editModalBackground}>
-        <div className={css.editModalContainer}>
-          <div className={css.titleCloseBtn}>
-            <button onClick={handleCloseModal}>x</button>
-          </div>
-          <h2 className={css.editModalHeader}>
-            Please edit selected transaction
-          </h2>
-          <div>
-            <SwitchCheckbox
-              prevCategory={prevCategory}
-              prevComment={prevComment}
-              prevType={prevType}
-              prevSum={prevSum}
-              isOn={checked}
-              handleToggle={handleToggle}
-              id={id}
-              updateBalance={updateBalance}
-              updateTransactions={updateTransactions}
-              setOpenEditModal={setOpenEditModal}
-            />
-          </div>
-          <div className={css.modalFooter}>
-            <button
-              onClick={handleCloseModal}
-              className={css.modalCancelButton}
-            >
-              CANCEL
-            </button>
+    <>
+      <Modal isOpen={true} onRequestClose={handleCloseModal} className={css.editModalBackground} overlayClassName={css.editModalOverlay}>
+        <Header />
+        <div className={css.editModalBackground}>
+          <div className={css.editModalContainer}>
+            <div className={css.titleCloseBtn}>
+              <button onClick={handleCloseModal}></button>
+            </div>
+            <h2 className={css.editModalHeader}>Please edit selected transaction</h2>
+            <div>
+              <SwitchCheckbox prevCategory={prevCategory} prevComment={prevComment} prevType={prevType} prevSum={prevSum} isOn={checked} handleToggle={handleToggle} id={id} updateBalance={updateBalance} updateTransactions={updateTransactions} setOpenEditModal={false} />
+            </div>
+            <div className={css.modalFooter}>
+              <button onClick={handleCloseModal} className={css.modalCancelButton}>
+                CANCEL
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-    </Modal>
+      </Modal>
+    </>
   );
 }
 
